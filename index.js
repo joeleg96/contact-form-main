6e7186378faede7752cd6e7a696c28fa9b048e53
Joel Eguiza <joeleg96@gmail.com>
2025-05-06 15:34:39 -0600

    updated js to allow for radio buttons to be checked when clicking button text and button container. updated consent box to be toggled upon clicking consent text

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const generalEnquiryButton = $(".general-enquiry-btn");
+const supportButton = $(".support-btn");
+const consentButton = $("#consent-btn");
+
+// General Enquiry Button
+$(".enquiry-text").on("click", function() {
+   $(generalEnquiryButton)[0].checked = true;
+});
+
 $(".general-enquiry-container").on("click", function() {
-    $(".general-enquiry-btn").checked = true;
-})
+    $(generalEnquiryButton)[0].checked = true;
+});
+
+// Support Button
+$(".support-text").on("click", function() {
+    $(supportButton)[0].checked = true;
+});
+
+$(".support-container").on("click", function() {
+    $(supportButton)[0].checked = true;
+});
+
+// Consent Button
+$(".consent-text").on("click", function() {
+    console.log(consentButton);
+    $(consentButton)[0].checked = !consentButton[0].checked;   
+});
