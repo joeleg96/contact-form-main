30e6007cfdd81cefb8608cafabf4e474075be00c
Joel Eguiza <joeleg96@gmail.com>
2025-05-08 12:25:28 -0600

    updated focus states, and error states formatting

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const consentButton = $("#consent-btn");
 // General Enquiry Button
 $(".enquiry-text").on("click", function() {
    $(generalEnquiryButton)[0].checked = true;
+   $(".support-container").removeClass("green-background");
+   $(".general-enquiry-container").addClass("green-background");
 });
 
 $(".general-enquiry-container").on("click", function() {
     $(generalEnquiryButton)[0].checked = true;
+    $(".support-container").removeClass("green-background");
+    $(".general-enquiry-container").addClass("green-background");
 });
 
 // Support Button
 $(".support-text").on("click", function() {
     $(supportButton)[0].checked = true;
+    $(".general-enquiry-container").removeClass("green-background");
+    $(".support-container").addClass("green-background");
 });
 
 $(".support-container").on("click", function() {
     $(supportButton)[0].checked = true;
+    $(".general-enquiry-container").removeClass("green-background");
+    $(".support-container").addClass("green-background");
 });
 
 // Consent Button
@@ -25,3 +33,4 @@ $(".consent-text").on("click", function() {
     console.log(consentButton);
     $(consentButton)[0].checked = !consentButton[0].checked;   
 });
+
