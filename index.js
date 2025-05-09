fb3a95b844a0a647912a74e78f46cb3241d5c3f5
Joel Eguiza <joeleg96@gmail.com>
2025-05-09 13:23:46 -0600

    fixed validation for query buttons and consent button

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const generalEnquiryButton = $(".general-enquiry-btn");
 const supportButton = $(".support-btn");
 const consentButton = $("#consent-btn");
-const queryButton = $(".radio-btn");
+const queryButton = $(".radio-btn, .enquiry-text, .general-enquiry-container, .support-text, .support-container");
+const radioButton = $(".radio-btn");
 
-// General Enquiry Button
+// General Enquiry
 $(".enquiry-text").on("click", function() {
    $(generalEnquiryButton)[0].checked = true;
    $(".support-container").removeClass("green-background");
@@ -16,7 +17,7 @@ $(".general-enquiry-container").on("click", function() {
     $(".general-enquiry-container").addClass("green-background");
 });
 
-// Support Button
+// Support Text
 $(".support-text").on("click", function() {
     $(supportButton)[0].checked = true;
     $(".general-enquiry-container").removeClass("green-background");
@@ -29,11 +30,13 @@ $(".support-container").on("click", function() {
     $(".support-container").addClass("green-background");
 });
 
-// Consent Button
+// Consent Text
 $(".consent-text").on("click", function() {
     console.log(consentButton);
     $(consentButton)[0].checked = !consentButton[0].checked;   
 });
+
+
 
 $(document).ready(function() {
     $("#first-name").on("blur", function() {
@@ -72,13 +75,17 @@ $(document).ready(function() {
         }
     })
 
-    // $(queryButton).on("click", function() {
-    //     $(".query-error").addClass("hide");
-    // })
+    $(queryButton).on("click", function() {
+      if ($(generalEnquiryButton).prop("checked") || $(supportButton).prop("checked")) {
+        $(".query-error").addClass("hide");
+      }
+    })
 
-    // $(consentButton).on("click", function() {
-    //     $(".consent-error").addClass("hide");
-    // })
+    $(consentButton).on("click", function() {
+      if ($(this).prop("checked")) {
+        $(".consent-error").addClass("hide");
+      } 
+    });
 
     $("#my-form").submit(function(event) {
       let isValid = true;
@@ -101,15 +108,14 @@ $(document).ready(function() {
         $(".message-input-error").removeClass("hide");
         isValid = false;
       }
-    //   if (!queryButton.checked) {
-    //     $(".query-error").removeClass("hide");
-    //     isValid = false;
-    //   }
-    //   if (!consentButton.checked) {
-    //      $(".consent-error").removeClass("hide");
-    //      isValid = false;
-    //   } 
-    
+      if (!generalEnquiryButton.prop("checked") && !supportButton.prop("checked")) {
+        $(".query-error").removeClass("hide");
+         isValid = false;
+      }
+      if (!consentButton.prop("checked")) {
+        $(".consent-error").removeClass("hide");
+         isValid = false;
+      } 
       if (!isValid) {
         event.preventDefault();
       } else {
